6faa5eeea8eab78b4c4650e83d4b49e843f3ac6a
Chen Xuanyu <1292557179@qq.com>
2024-05-09 07:20:57 -0700

    baseline completed & feature extraction for plan

---
+++ src/plan_based/database_explore.py
@@ -0,0 +1,9 @@
+import json
+
+# Exploring the raw dataset.
+file_name = "../../datasets/plans/parsed/workload_5k_s1_c8220.json"
+with open(file_name, "r") as f:
+    raw_accidents_normal_data = json.load(f)
+print(raw_accidents_normal_data["parsed_plans"][0].keys())
+print(len(raw_accidents_normal_data["parsed_plans"][0]["children"][0]["children"][0]["children"][0]["children"]))
+# print(raw_accidents_normal_data["parsed_plans"][0]["children"])
